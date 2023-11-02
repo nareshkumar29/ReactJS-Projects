3ab6146df2a8ebadb88cc201801c7af0e7e7c6a0
Naresh Kumar <121565671+nareshkumar29@users.noreply.github.com>
2023-11-02 21:35:57 +0530

    Add files via upload

---
+++ Project - 5 Random Joke Generator/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Joke from "./components/Joke";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Random Joke Generator</h1>
+      <Joke/>      
+    </div>
+  );
+}
+
+export default App;
